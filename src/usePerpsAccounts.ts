ad17336e8457e1f451b3d9fbde3f9536567ee640
Volodymyr Derunov <vderunov@gmail.com>
2024-09-26 23:57:40 +0300

    Add usePerpsAccounts (#4)
    * Add usePerpsAccounts
    
    * Add error handling with useErrorParser in usePerpsAccounts

---
+++ src/usePerpsAccounts.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { ethers } from 'ethers';
+import { useErrorParser } from './useErrorParser';
+import { useImportContract } from './useImports';
+import { useSynthetix } from './useSynthetix';
+
+export function usePerpsAccounts({
+  provider,
+  walletAddress,
+}: {
+  provider?: ethers.providers.BaseProvider;
+  walletAddress?: string;
+}) {
+  const { chainId } = useSynthetix();
+  const { data: PerpsAccountProxyContract } = useImportContract('PerpsAccountProxy');
+  const errorParser = useErrorParser();
+
+  return useQuery({
+    enabled: Boolean(chainId && provider && walletAddress && PerpsAccountProxyContract?.address),
+    queryKey: [chainId, 'Perps Accounts', { PerpsAccountProxy: PerpsAccountProxyContract?.address }, { ownerAddress: walletAddress }],
+    queryFn: async () => {
+      if (!(chainId && provider && walletAddress && PerpsAccountProxyContract?.address)) throw 'OMFG';
+      const PerpsAccountProxy = new ethers.Contract(PerpsAccountProxyContract.address, PerpsAccountProxyContract.abi, provider);
+      const numberOfAccountTokens = await PerpsAccountProxy.balanceOf(walletAddress);
+      if (numberOfAccountTokens.eq(0)) {
+        // No accounts created yet
+        return [];
+      }
+      const accountIndexes = Array.from(Array(numberOfAccountTokens.toNumber()).keys());
+      const accounts = await Promise.all(accountIndexes.map((i) => PerpsAccountProxy.tokenOfOwnerByIndex(walletAddress, i)));
+      return accounts;
+    },
+    select: (accounts) => accounts.map((accountId) => ethers.BigNumber.from(accountId)),
+    throwOnError: (error) => {
+      // TODO: show toast
+      errorParser(error);
+      return false;
+    },
+  });
+}
