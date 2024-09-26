9e5e84d8f7eec464d3939f0a08a391dc9ddd399e
Volodymyr Derunov <vderunov@gmail.com>
2024-09-26 23:29:45 +0300

    Add usePerpsMetadata (#3)
    * Add usePerpsMetadata
    
    * Rename parameter marketId to perpsMarketId.
    
    * Add error handling with errorParser in usePerpsMetadata

---
+++ src/usePerpsMetadata.ts
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
+import { ethers } from 'ethers';
+import { useErrorParser } from './useErrorParser';
+import { useImportContract } from './useImports';
+import { useSynthetix } from './useSynthetix';
+
+export function usePerpsMetadata({
+  provider,
+  perpsMarketId,
+}: { provider?: ethers.providers.BaseProvider; perpsMarketId?: ethers.BigNumber }) {
+  const { chainId } = useSynthetix();
+  const { data: PerpsMarketProxyContract } = useImportContract('PerpsMarketProxy');
+  const errorParser = useErrorParser();
+
+  return useQuery<{
+    name: string;
+    symbol: string;
+  }>({
+    enabled: Boolean(chainId && provider && perpsMarketId && PerpsMarketProxyContract?.address),
+    queryKey: [
+      chainId,
+      'Perps Metadata',
+      { PerpsMarketProxy: PerpsMarketProxyContract?.address },
+      { perpsMarketId: perpsMarketId?.toString() },
+    ],
+    queryFn: async () => {
+      if (!(chainId && provider && perpsMarketId && PerpsMarketProxyContract?.address)) {
+        throw 'OMFG';
+      }
+
+      const PerpsMarketProxy = new ethers.Contract(PerpsMarketProxyContract.address, PerpsMarketProxyContract.abi, provider);
+      const { symbol, name } = await PerpsMarketProxy.metadata(perpsMarketId);
+      console.log({ symbol, name });
+      return { symbol, name };
+    },
+    throwOnError: (error) => {
+      // TODO: show toast
+      errorParser(error);
+      return false;
+    },
+  });
+}
