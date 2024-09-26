c6db982a2d02d5dd88bb4eff146038d68dffc29d
Volodymyr Derunov <vderunov@gmail.com>
2024-09-26 12:05:57 +0300

    Rename parameter marketId to perpsMarketId.

--- src/usePerpsMetadata.ts
+++ src/usePerpsMetadata.ts
@@ -3,25 +3,33 @@ import { ethers } from 'ethers';
 import { useImportContract } from './useImports';
 import { useSynthetix } from './useSynthetix';
 
-interface PerpsMetadata {
-  name: string;
-  symbol: string;
-}
-
-export function usePerpsMetadata({ provider, marketId }: { provider?: ethers.providers.BaseProvider; marketId?: ethers.BigNumber }) {
+export function usePerpsMetadata({
+  provider,
+  perpsMarketId,
+}: { provider?: ethers.providers.BaseProvider; perpsMarketId?: ethers.BigNumber }) {
   const { chainId } = useSynthetix();
   const { data: PerpsMarketProxyContract } = useImportContract('PerpsMarketProxy');
 
-  return useQuery<PerpsMetadata>({
-    enabled: Boolean(chainId && provider && marketId && PerpsMarketProxyContract?.address),
-    queryKey: [chainId, 'Perps Metadata', { PerpsMarketProxy: PerpsMarketProxyContract?.address }, { marketId: marketId?.toString() }],
+  return useQuery<{
+    name: string;
+    symbol: string;
+  }>({
+    enabled: Boolean(chainId && provider && perpsMarketId && PerpsMarketProxyContract?.address),
+    queryKey: [
+      chainId,
+      'Perps Metadata',
+      { PerpsMarketProxy: PerpsMarketProxyContract?.address },
+      { perpsMarketId: perpsMarketId?.toString() },
+    ],
     queryFn: async () => {
-      if (!(chainId && provider && marketId && PerpsMarketProxyContract?.address)) {
+      if (!(chainId && provider && perpsMarketId && PerpsMarketProxyContract?.address)) {
         throw 'OMFG';
       }
 
       const PerpsMarketProxy = new ethers.Contract(PerpsMarketProxyContract.address, PerpsMarketProxyContract.abi, provider);
-      return PerpsMarketProxy.metadata(marketId);
+      const { symbol, name } = await PerpsMarketProxy.metadata(perpsMarketId);
+
+      return { symbol, name };
     },
   });
 }
